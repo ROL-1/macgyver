34749b8a2e1e719fff49634d621b430dafb25fc1
ROL-1 <loicromero1@gmail.com>
2020-04-22 23:08:56 +0200

    cleaned, use player.py and add win condition

--- player.py
+++ player.py
@@ -1,32 +1,30 @@
-# """Player's display and functions file for MacGiver Maze game."""
-# import numpy as np
-# import pygame
-# from pygame.locals import *
-# from config import *
-# from maze import *
+"""Player functions file for MacGiver Maze game."""
+import numpy as np
+import pygame
+from pygame.locals import *
+from config import *
+from maze import *
 
-# class Player:
-#     """Rules for player movements."""
+class Player:
+    """Player start position and rules for movements."""
+    
+    def __init__(self,level):
+        """Player start position."""
+        self.level = level
+        self.x = self.level.perso_start_position[0]
+        self.y = self.level.perso_start_position[1]
 
-#     def __init__(self):
-#         """???"""
-
-#     def movement(self,move):
-#         """Rules for player movements."""
-#         if move == 'up':
-#             move_up = level.maze[position_perso[0]][position_perso[1]-1]
-#             if move_up != 'W':
-#                 position_perso = move_up    
-#   
-#         return position_perso           
-                
-
-                
-            
-
-
-          
-            
-          
-
-       
+    def movement(self,move):
+        """Rules for player movements."""
+        if move == 'up':
+            if self.level.maze[self.y-1][self.x] != 'W':
+                self.y -= 1
+        if move == 'down':
+            if self.level.maze[self.y+1][self.x] != 'W':
+                self.y += 1 
+        if move == 'left':
+            if self.level.maze[self.y][self.x-1] != 'W':
+                self.x -= 1
+        if move == 'right':
+            if self.level.maze[self.y][self.x+1] != 'W':
+                self.x += 1
