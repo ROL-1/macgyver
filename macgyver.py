5f6a59942d836f2c70cdc54f0ba84535ce29ed5a
ROL-1 <loicromero1@gmail.com>
2020-04-22 20:32:00 +0200

    wip work1

--- macgyver.py
+++ macgyver.py
@@ -4,6 +4,7 @@ from player import *
 import pygame
 from pygame.locals import *
 from config import *
+import os
 
 
 def main():
@@ -16,13 +17,18 @@ def main():
     level.load_maze()
     # Display the maze
     level.display_maze(window)
-    # Generate player's perso
-    player = Perso(window)
+    level.perso_start_position(window)
+    # Player movements
+    player = Player(level)   
+
+
 
 ################################## GAME LOOP ################################
     pygame.key.set_repeat(key_set_repeat_delay,key_set_repeat_interval)
     pygame.time.Clock().tick(time_clock_tick)
     loop = 1
+    sprite = sprites_size
+    position_perso = level.perso_start_position
     while loop:
         for event in pygame.event.get():
             # Close window
@@ -30,20 +36,25 @@ def main():
                 loop = 0
             # Keyboard reactions    
             if event.type == KEYDOWN:
+                # print(position_perso) # To Clean
                 if event.key == K_UP:
-                    player.position_perso = player.position_perso.move(0, -sprites_size)
+                    # position_perso = (position_perso[0], position_perso[1]-1)                    
+                    position_perso = player.movement('up')
                 if event.key == K_DOWN:
-                    player.position_perso = player.position_perso.move(0, sprites_size)
+                    # position_perso = (position_perso[0], position_perso[1]+1)
+                    position_perso = player.movement('down')
                 if event.key == K_LEFT:
-                    player.position_perso = player.position_perso.move(-sprites_size, 0)
+                    # position_perso = (position_perso[0]-1, position_perso[1])
+                    position_perso = player.movement('left')
                 if event.key == K_RIGHT:
-                    player.position_perso = player.position_perso.move(sprites_size, 0)
+                    # position_perso = (position_perso[0]+1, position_perso[1])
+                    position_perso = player.movement('right')
         # Re-paste images
         background = pygame.image.load(background_file).convert()
         perso = pygame.image.load(perso_file).convert_alpha()
         window.blit(background, (0, 0))
         level.display_maze(window)
-        window.blit(perso, (player.position_perso))
+        window.blit(perso, (player.x*sprite, player.y*sprite))
         # Refresh
         pygame.display.flip()
 ################################# END GAME LOOP ##############################
--- player.py
+++ player.py
@@ -1,14 +1,32 @@
-"""Player's display and functions file for MacGiver Maze game."""
-import numpy as np
-import pygame
-from pygame.locals import *
+# """Player's display and functions file for MacGiver Maze game."""
+# import numpy as np
+# import pygame
+# from pygame.locals import *
+# from config import *
+# from maze import *
 
-class Perso:
-    """Display player's perso, first position."""
+# class Player:
+#     """Rules for player movements."""
 
-    def __init__(self, window):
-        """Load and display image for player's perso."""
-        perso = pygame.image.load("images/macgyver.png").convert_alpha()
-        position_perso = perso.get_rect()
-        position_perso.center = (225, 225)
-        self.position_perso = position_perso
+#     def __init__(self):
+#         """???"""
+
+#     def movement(self,move):
+#         """Rules for player movements."""
+#         if move == 'up':
+#             move_up = level.maze[position_perso[0]][position_perso[1]-1]
+#             if move_up != 'W':
+#                 position_perso = move_up    
+#   
+#         return position_perso           
+                
+
+                
+            
+
+
+          
+            
+          
+
+       
