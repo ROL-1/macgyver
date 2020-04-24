883f08ab4710fff148dd912e552167d06c2f2753
ROL-1 <loicromero1@gmail.com>
2020-04-24 18:19:12 +0200

    cleaning flake8

--- config.py
+++ config.py
@@ -1,34 +1,36 @@
 """References and parameters for files."""
 
 # maze construction file 
-level_config1_file = "level_config_1.json"
+level_config_file = "level_config_1.json" 
 
 
 #################### Images ######################
 # Maze
-background_file = "images/background.jpg"
-outdoor_file = "images/outdoor.png"
+background_file = "images/background.jpg" 
+# Player
+perso_file = "images/macgyver.png" 
+
+outdoor_file = "images/outdoor.png" 
 wall_file = "images/wall.png"
 # BadGuy and Objects
 badguy_file = "images/badguy.png"
-ether_file = "images/ether.png"
-needle_file = "images/needle.png"
-pipe_file = "images/pipe.png"
-# Player
-perso_file = "images/macgyver.png"
+obj1_file = "images/ether.png"
+obj2_file = "images/needle.png"
+obj3_file = "images/pipe.png"
+
 
 ####################################################
 
-
 # Sprites sizes (pixels)
-sprites_size = 30
+sprite = 30
+
+
 
 # Window size (x pixels, y pixels)
 window_size = (450, 450)
 
-
 ################ Pygame parameters ################
-key_set_repeat_delay = 400   # (milliseconds)
+key_set_repeat_delay = 400   # (milliseconds) 
 key_set_repeat_interval = 30 # (milliseconds)
 time_clock_tick = (30)       # (milliseconds)
 
--- maze.py
+++ maze.py
@@ -2,7 +2,8 @@
 import pygame
 from pygame.locals import *
 from config import *
-from player import *
+from player import Player
+from config import sprite
 
 
 class Create_maze:
@@ -10,11 +11,11 @@ class Create_maze:
 
     def __init__(self):
         """Load maze file."""
-        self.MAZE_FILE = level_config1_file
+        self.MAZE_FILE = level_config_file
         self.badguy_sleeping = False
-        self.ether_looted = False
-        self.needle_looted = False
-        self.pipe_looted = False
+        self.obj1_looted = False
+        self.obj2_looted = False
+        self.obj3_looted = False
 
     def load_maze(self):
         """Read maze file and return the maze frame (list type)."""
@@ -38,39 +39,38 @@ class Create_maze:
         wall = pygame.image.load(wall_file).convert_alpha()
         # BadGuy and Objects files
         badguy = pygame.image.load(badguy_file).convert_alpha()
-        ether = pygame.image.load(ether_file).convert_alpha()
-        needle = pygame.image.load(needle_file).convert_alpha()
-        pipe = pygame.image.load(pipe_file).convert_alpha()
+        obj1 = pygame.image.load(obj1_file).convert_alpha()
+        obj2 = pygame.image.load(obj2_file).convert_alpha()
+        obj3 = pygame.image.load(obj3_file).convert_alpha()
 
-        sprite = sprites_size
         for x in range(len(self.maze)):
             for y in range(len(self.maze)):
                 if self.maze[y][x] == 'W':
                     window.blit(wall, (sprite*x, sprite*y))
                 if self.maze[y][x] == 'O':
-                    self.position_outdoor = (x, y)
+                    self.coord_outdoor = (x, y)
                     window.blit(outdoor, (sprite*x, sprite*y))
                 if self.maze[y][x] == 'G':
-                    self.position_badguy = (x, y)
-                    if self.badguy_sleeping != True:
+                    self.coord_badguy = (x, y)
+                    if self.badguy_sleeping is not True:
                         window.blit(badguy, (sprite*x, sprite*y))
                 if self.maze[y][x] == 'T':
-                    self.position_ether = (x, y)
-                    if self.ether_looted != True:
-                        window.blit(ether, (sprite*x, sprite*y))
+                    self.coord_obj1 = (x, y)
+                    if self.obj1_looted is not True:
+                        window.blit(obj1, (sprite*x, sprite*y))
                 if self.maze[y][x] == 'N':
-                    self.position_needle = (x, y)
-                    if self.needle_looted != True:
-                        window.blit(needle, (sprite*x, sprite*y))
+                    self.coord_obj2 = (x, y)
+                    if self.obj2_looted is not True:
+                        window.blit(obj2, (sprite*x, sprite*y))
                 if self.maze[y][x] == 'P':
-                    self.position_pipe = (x, y)
-                    if self.pipe_looted != True:
-                        window.blit(pipe, (sprite*x, sprite*y))
+                    self.coord_obj3 = (x, y)
+                    if self.obj3_looted is not True:
+                        window.blit(obj3, (sprite*x, sprite*y))
 
-    def perso_start_position(self, window):
+    def perso_start_coord(self, window):
         """Generate coordinates for perso start position."""
         for x in range(len(self.maze)):
             for y in range(len(self.maze)):
                 if self.maze[y][x] == 'M':
-                        perso_start_position = (x, y)
-        self.perso_start_position = perso_start_position
+                    perso_start_coord = (x, y)
+        self.perso_start_coord = perso_start_coord
