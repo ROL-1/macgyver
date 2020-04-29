042ce4e133111e98bc1a4732cf6a0e5b607a8112
ROL-1 <loicromero1@gmail.com>
2020-04-29 18:31:04 +0200

    rework working with display

--- macgyver.py
+++ macgyver.py
@@ -1,4 +1,5 @@
 """Main file for MacGiver maze game."""
+from display import Display
 from maze import Maze
 from player import Player
 import pygame
@@ -15,13 +16,10 @@ def main():
     # Load window
     window = pygame.display.set_mode(window_size)
     # Load & generate the maze from the file
-    level = Maze()   
-    # Display the maze
-    level.display_maze(window)
-    level.perso_start_coord(window)
-    # Player movements
+    level = Maze()    
     player = Player(level)
-    inventory = []
+    # Display the maze
+    display = Display(level,player)
 
 # GAME LOOP ###################################################################
     pygame.key.set_repeat(key_set_repeat_delay, key_set_repeat_interval)
@@ -40,23 +38,23 @@ def main():
                     if event.key == move:
                         player.movement(move)
 
-        # Create Inventory
+        # Check Inventory
         player.inventory(level)
-        player.inventory_list = inventory
-
+        # player.inventory_list = inventory
+        print(player.inventory_list) #To Clean
         # Meeting BadGuy
         if (player.x, player.y) == level.coord_badguy:
             # Check inventory
-            if len(inventory) != nb_obj:
+            if len(player.inventory_list) != nb_obj:
                 print('YOU LOOSE.')
                 loop = 0
             else:
-                level.badguy_sleeping = True
+                display.badguy_sleeping = True
 
         # Check Exit
         if (player.x, player.y) == level.coord_outdoor:
             # Check if badguy is sleeping
-            if level.badguy_sleeping is True:
+            if display.badguy_sleeping is True:
                 print('YOU WIN!')
                 loop = 0
             else:
@@ -64,14 +62,15 @@ def main():
                 loop = 0
 
         # Re-paste images
-        background = pygame.image.load(background_file).convert()
-        perso = pygame.image.load(perso_file).convert_alpha()
-        window.blit(background, (0, 0))
-        level.display_maze(window)
-        level.display_objects(window, inventory)
-        window.blit(perso, (player.x*sprite, player.y*sprite))
-        # Refresh
-        pygame.display.flip()
+        display.repaste_display(level,player)
+        # background = pygame.image.load(background_file).convert()
+        # perso = pygame.image.load(perso_file).convert_alpha()
+        # window.blit(background, (0, 0))
+        # level.display_maze(window)
+        # level.display_objects(window, inventory)
+        # window.blit(perso, (player.x*sprite, player.y*sprite))
+        # # Refresh
+        # pygame.display.flip()
 
 # END GAME LOOP ###############################################################
 
